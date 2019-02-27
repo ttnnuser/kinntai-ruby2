4269ebd674d97338e83e036de43bdf39405d9931
EC2 Default User <ec2-user@ip-172-31-3-13.us-east-2.compute.internal>
2019-02-27 13:26:56 +0000

    Finish basic infomation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/edit-basic-info/:id', to: 'users#edit_basic_info', as: :basic_info
+  patch 'update-basic-info', to: 'users#update_basic_info'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
